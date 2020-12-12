46b6f660dd18e4d9ab0def3835c64f5379868dbd
Khusharth <khusharth19@gmail.com>
2020-12-12 18:30:31 +0530

    :package: Adding data

---
+++ src/data.js
@@ -0,0 +1,134 @@
+export const action = [
+    {
+        name: "One Piece",
+        description:
+            "A swashbuckling adventure featuring duels with supernatural powers, fun characters, voyages and tons of pirates, One Piece is one of the most popular anime series out there. It will certainly fulfill any action craving you have...",
+        link: "https://myanimelist.net/anime/21/One_Piece?q=one%20pi",
+    },
+    {
+        name: "Naruto: Shippuuden",
+        description:
+            "Problem student Naruto was never popular or talented as a ninja. That is, until he discovers the hidden power of the Nine-Tailed Fox sealed within him. Even though Naruto's physical strength is due to the Fox...",
+        link: "https://myanimelist.net/anime/1735/Naruto__Shippuuden",
+    },
+    {
+        name: "Hunter X Hunter",
+        description:
+            "Hunter x Hunter has action, great characters and even scenes that will pull at your heartstrings. Each episode will have you laughing, crying, or biting your nails during great action scenes!...",
+        link:
+            "https://myanimelist.net/anime/11061/Hunter_x_Hunter_(2011)?q=hunte",
+    },
+    {
+        name: "Attack on Titan",
+        description:
+            "Attack on Titan took the anime community by storm. It definitely deserves credit for being one of the best anime series within the past five years. Amazing battle equipment, badass soldiers, cliff-hangers, and some pretty...",
+        link: "https://myanimelist.net/anime/16498/Shingeki_no_Kyojin?q=shinge",
+    },
+    {
+        name: "Fullmetal Alchemist",
+        description:
+            "Fullmetal Alchemist not only has tons of amazing action scenes, but also contains fun, memorable characters, tear-jerking scenes and beautiful music. This is a must watch anime series for anime fans alike...",
+        link: "https://myanimelist.net/anime/121/Fullmetal_Alchemist",
+    },
+];
+
+export const comedy = [
+    {
+        name: "Gintama",
+        description:
+            "The world of feudal Japan – samurai, yakuza… alien overlords?! With our protagonist just trying to make a few bucks in this brave new world, Gintama offers a refreshingly silly take on the samurai genre...",
+        link: "https://myanimelist.net/anime/918/Gintama",
+    },
+    {
+        name: "Hataraku Maou-sama!",
+        description:
+            "His dreams of world conquest shattered, the devil himself is banished to modern-day Tokyo, where he must do the only thing a disgraced demon lord can to survive – get a job at a fast food restaurant!...",
+        link: "https://myanimelist.net/anime/15809/Hataraku_Maou-sama!",
+    },
+    {
+        name: "Baka to Test to Shoukanjuu",
+        description:
+            "Fumizuki Academy is no ordinary school – students are divided into a rigid caste system based on test results. Under the leadership of Akihisa, Class F attempt to rally and seize the best facilities for themselves...",
+        link: "https://myanimelist.net/anime/6347/Baka_to_Test_to_Shoukanjuu",
+    },
+    {
+        name: "Kono Subarashii Sekai ni Shukufuku wo!",
+        description:
+            "After dying a laughable and pathetic death on his way back from buying a game, high school student and recluse Kazuma Satou finds himself sitting before a beautiful but obnoxious goddess named Aqua...",
+        link:
+            "https://myanimelist.net/anime/30831/Kono_Subarashii_Sekai_ni_Shukufuku_wo",
+    },
+    {
+        name: "Asobi Asobase",
+        description:
+            "During recess, Olivia, a foreign transfer student who doesn't know English, plays a game of 'look-the-other-way' with Hanako Honda, a loud-mouthed airhead. Their rowdy behavior spurs the ire of Kasumi Nomura, a deadpan loner...",
+        link: "https://myanimelist.net/anime/37171/Asobi_Asobase",
+    },
+];
+
+export const romance = [
+    {
+        name: "You Lie in April",
+        description:
+            "Music can impact our lives in amazing ways. This romance anime portrays this beautiful message through its main characters and their journey to let go of the past and strive to grasp a brighter future...",
+        link: "https://myanimelist.net/anime/23273/Shigatsu_wa_Kimi_no_Uso",
+    },
+    {
+        name: "Kaichou wa Maid-sama!",
+        description:
+            "Misaki is the class president, known for being strict and cold, who hides one very dark secret: she works part-time in a maid cafe dressed up in a cute maid costume! That is until she is...",
+        link: "https://myanimelist.net/anime/7054/Kaichou_wa_Maid-sama",
+    },
+    {
+        name: "Anohana",
+        description:
+            "You never forget your first love and special childhood memories. Anohana explores how they can affect us in the present and just how important they are to us...",
+        link:
+            "https://myanimelist.net/anime/9989/Ano_Hi_Mita_Hana_no_Namae_wo_Bokutachi_wa_Mada_Shiranai",
+    },
+    {
+        name: "Hotarubi no Morie e",
+        description:
+            "Intrigued by the tale of a mountain god, six-year-old Hotaru Takegawa loses her way in the ancient forest while visiting her uncle. Exhausted and desperate for help, Hotaru is thrilled to...",
+        link: "https://myanimelist.net/anime/10408/Hotarubi_no_Mori_e",
+    },
+    {
+        name: "The Pet Girl of Sakurasou",
+        description:
+            "When abandoned kittens and his good conscience force second year Sorata Kanda to move into Suimei High School’s infamous Sakura Hall, the satellite dorm and its eccentric, misfit residents turn his life upside down... ",
+        link: "https://myanimelist.net/anime/13759/Sakura-sou_no_Pet_na_Kanojo",
+    },
+];
+
+export const thriller = [
+    {
+        name: "Steins Gate",
+        description:
+            "Rintarou Okabe and his two friends spend their days working as mad scientists, inventing crazy gadgets for thrills. When an experiment unexpectedly gives the trio the ability to change the very course of history...",
+        link: "https://myanimelist.net/anime/9253/Steins_Gate",
+    },
+    {
+        name: "Death Note",
+        description:
+            "When young prodigy Light Yagami finds a mysterious notebook that can kill those whose names are written inside, he uses the power to begin shaping his own ideal world. As the body count rises...",
+        link: "https://myanimelist.net/anime/1535/Death_Note",
+    },
+    {
+        name: "Death Parade",
+        description:
+            "What we would call limbo - the place in between heaven and hell - materializes itself as a bar, where the dead must play a game. Those who win are reincarnated while the losers fall into oblivion forever...",
+        link: "https://myanimelist.net/anime/28223/Death_Parade",
+    },
+    {
+        name: "Future Diary",
+        description:
+            "Deus Ex Machina, the god of time and space, has gifted 12 young individuals with magical diaries. Each diary holds a special power that their holders must use to win a battle royale...",
+        link: "https://myanimelist.net/anime/10620/Mirai_Nikki_TV",
+    },
+    {
+        name: "Another",
+        description:
+            "In the gloomy atmosphere of Yomiyama North Middle School, Kouichi Sakakibara takes an interest in his classmate Mei Misaki despite cryptic warnings from the people around him...",
+        link: "https://myanimelist.net/anime/11111/Another",
+    },
+];
